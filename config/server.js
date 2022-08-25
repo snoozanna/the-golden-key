f122f094eec960b6b63053eefe4be320c7f024e3
suzannahurst <hurstsuzanna@gmail.com>
2022-08-25 12:46:38 +0100

    server.js change

--- config/server.js
+++ config/server.js
@@ -1,13 +1,13 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   admin: {
-//     auth: {
-//       secret: env('ADMIN_JWT_SECRET', '5eb776e394c91183196678010a5421e5'),
-//     },
-//   },
-// });
 module.exports = ({ env }) => ({
-  host: env("APP_HOST", "0.0.0.0"),
-  port: env.int("NODE_PORT", 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "5eb776e394c91183196678010a5421e5"),
+    },
+  },
 });
+// module.exports = ({ env }) => ({
+//   host: env("APP_HOST", "0.0.0.0"),
+//   port: env.int("NODE_PORT", 1337),
+// });
