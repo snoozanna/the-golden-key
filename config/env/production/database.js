f32994d2f2511baaa61600916766771397c9d82e
suzannahurst <hurstsuzanna@gmail.com>
2022-08-28 19:27:20 +0100

    env is a fn

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -20,16 +20,23 @@
 // });
 
 module.exports = ({ env }) => {
-  console.log("env", env);
+  console.log(
+    "env",
+    env("DATABASE_HOST"),
+    env("DATABASE_PORT"),
+    env("DATABASE_NAME"),
+    env("USERNAME"),
+    env("PASSWORD")
+  );
   return {
     connection: {
       client: "postgres",
       connection: {
-        host: env(DATABASE_HOST),
-        port: env(DATABASE_PORT),
-        database: env(DATABASE_NAME),
-        user: env(USERNAME),
-        password: env(PASSWORD),
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("USERNAME"),
+        password: env("PASSWORD"),
         ssl: {
           rejectUnauthorized: false,
         },
