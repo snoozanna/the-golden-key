63bbacc74a7eff18d0a83319e47a1979ecbd9ea8
suzannahurst <hurstsuzanna@gmail.com>
2022-08-28 19:25:53 +0100

    env is a fn

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,5 @@
 // const parse = require("pg-connection-string").parse;
 // const config = parse(process.env.DATABASE_URL);
-const env = process.env;
 
 // console.log(DATABASE_URL);
 // module.exports = ({ env }) => ({
@@ -26,11 +25,11 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env.DATABASE_HOST,
-        port: env.DATABASE_PORT,
-        database: env.DATABASE_NAME,
-        user: env.USERNAME,
-        password: env.PASSWORD,
+        host: env(DATABASE_HOST),
+        port: env(DATABASE_PORT),
+        database: env(DATABASE_NAME),
+        user: env(USERNAME),
+        password: env(PASSWORD),
         ssl: {
           rejectUnauthorized: false,
         },
