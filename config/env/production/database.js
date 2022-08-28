6522e1222f74fbd09e34bbd27eb26df75509b9e6
suzannahurst <hurstsuzanna@gmail.com>
2022-08-28 17:49:40 +0100

    individual credentials

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,33 @@
-const DATABASE_URL =
-  "postgres://pfsqopewlozwvt:cad7dcafe6d2185f6180f0c5de7b2538b7f8b32816d4fe4ec394400ff577d72c@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/ddfjobo1mdn69u";
-const parse = require("pg-connection-string").parse;
+// const parse = require("pg-connection-string").parse;
 // const config = parse(process.env.DATABASE_URL);
-const config = parse(DATABASE_URL);
-console.log(DATABASE_URL);
+
+// console.log(DATABASE_URL);
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: config.host,
+//       port: config.port,
+//       database: config.database,
+//       user: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: config.DATABASE_HOST,
+      port: config.DATABASE_PORT,
+      database: config.DATABASE_NAME,
+      user: config.USERNAME,
+      password: config.PASSWORD,
       ssl: {
         rejectUnauthorized: false,
       },
